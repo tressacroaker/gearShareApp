3ef73e9768090ffe85e235a1f875d01bbeac7c12
tressacroaker <tressacroaker36@gmail.com>
2016-11-01 15:01:11 -0600

    gulp still broke

--- public/js/mainServ.js
+++ public/js/mainServ.js
@@ -1,15 +1,15 @@
 angular.module("gearApp")
 .service("mainServ",function($http){
-  this.postNewUser(user){
-    console.log(user);
-    return $http({
-      method: "POST",
-      url: "/login",
-      data:{
-        new User: user
-      }
-    })
-  };
+  // this.postNewUser(user){
+  //   console.log(user);
+  //   return $http({
+  //     method: "POST",
+  //     url: "/login",
+  //     data:user
+  //   }).then(function(response){
+  //       return response;
+  //   })
+  // };
 
 
 
